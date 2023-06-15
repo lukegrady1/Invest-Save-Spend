59d98149964e691b0882c1b790fef74be9a79597
lukegrady1 <lukegrady9@outlook.com>
2023-06-15 15:42:34 -0400

    changes

--- script.js
+++ script.js
@@ -1,34 +1,33 @@
-var savePercent = 20; // Hardcoded save percentage
-var spendPercent = 10; // Hardcoded spend percentage
-var investPercent = 70; // Hardcoded invest percentage
-
 function calculate() {
-  var totalAmount = parseFloat(document.getElementById('total-amount').value);
+  var totalAmount = parseFloat(document.getElementById('total-amount-input').value);
+  var investPercent = parseFloat(document.getElementById('invest-input').value);
+  var savePercent = parseFloat(document.getElementById('saving-input').value);
+  var spendPercent = parseFloat(document.getElementById('spending-input').value);
 
+  var investAmount = (totalAmount * investPercent) / 100;
   var saveAmount = (totalAmount * savePercent) / 100;
   var spendAmount = (totalAmount * spendPercent) / 100;
-  var investAmount = (totalAmount * investPercent) / 100;
 
+  document.getElementById('invest-amount').textContent = 'Invest: $' + investAmount.toFixed(2);
   document.getElementById('save-amount').textContent = 'Save: $' + saveAmount.toFixed(2);
   document.getElementById('spend-amount').textContent = 'Spend: $' + spendAmount.toFixed(2);
-  document.getElementById('invest-amount').textContent = 'Invest: $' + investAmount.toFixed(2);
 
   // Create the pie chart
   var ctx = document.getElementById('chart').getContext('2d');
   if (window.myChart) {
     // Update existing chart data
-    window.myChart.data.datasets[0].data = [saveAmount, spendAmount, investAmount];
+    window.myChart.data.datasets[0].data = [investAmount, saveAmount, spendAmount];
     window.myChart.update();
   } else {
     // Create new chart
     window.myChart = new Chart(ctx, {
       type: 'pie',
       data: {
-        labels: ['Save', 'Spend', 'Invest'],
+        labels: ['Invest', 'Save', 'Spend'],
         datasets: [{
           label: 'Amount',
-          data: [saveAmount, spendAmount, investAmount],
-          backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
+          data: [investAmount, saveAmount, spendAmount],
+          backgroundColor: ['#FFCE56', '#FF6384', '#36A2EB'],
         }]
       },
       options: {
@@ -54,5 +53,8 @@ function goBack() {
   chartContainer.innerHTML = '';
 
   // Clear input field
-  document.getElementById('total-amount').value = '';
+  document.getElementById('total-amount-input').value = '';
+  document.getElementById('invest-input').value = '';
+  document.getElementById('saving-input').value = '';
+  document.getElementById('spending-input').value = '';
 }
